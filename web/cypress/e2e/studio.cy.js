73797d319c3bae3847fd2fa7e859895e63d44d86
Patrick <patrick.jvc@gmail.com>
2025-08-22 11:31:06 +0100

    salvando alterações do projeto Cypress

---
+++ web/cypress/e2e/studio.cy.js
@@ -0,0 +1,16 @@
+describe('Studio', () => {
+  it('Exemplo do Cypress Studio', () => {
+    cy.visit('https://example.cypress.io')
+  })
+
+  /* ==== Test Created with Cypress Studio ==== */
+  it('Deve logar com sucesso', () => {
+    /* ==== Generated with Cypress Studio ==== */
+    cy.visit('http://localhost:3000')
+    cy.get('#email').type('papito@webdojo.com')
+    cy.get('#password').type('katana123')
+    cy.contains('button', 'Entrar').click()
+    cy.get('[data-cy="user-name"]').should('have.text', 'Fernando Papito');
+    /* ==== End Cypress Studio ==== */
+  });
+})
