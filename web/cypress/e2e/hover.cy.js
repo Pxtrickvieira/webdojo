f1663237b208dfddc82b625dfb4ead3fca45fd64
Patrick <patrick.jvc@gmail.com>
2025-08-22 12:13:58 +0100

    criando teste de links

--- web/cypress/e2e/hover.cy.js
+++ web/cypress/e2e/hover.cy.js
@@ -3,7 +3,9 @@ describe('Simulando Mouseover', () => {
         cy.start()
         cy.submitLoginForm('papito@webdojo.com', 'katana123')
 
+        cy.contains('Isso é Mouseover!').should('not.exist')
         cy.get('[data-cy="instagram-link"]').realHover()
+        cy.contains('Isso é Mouseover!').should('exist')
 
     })
 })
